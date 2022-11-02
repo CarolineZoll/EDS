e41248b18fd4495ff5b9ec2857cd1b8d5edc49c3
CarolineZoll <114507886+CarolineZoll@users.noreply.github.com>
2022-11-02 21:16:04 +0100

    new notebook

--- simulation.py
+++ simulation.py
@@ -1,56 +1,88 @@
 import simpy
 import random
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import poisson
 
-SCHEDULE_T=1 #Clock des Schedulers 
+SCHEDULE_T=2 #Clock des Schedulers 
 
 #function to monitor the level of the different queues
-def monitor(queue,monitor): 
+def monitor(queue,monitor,env): 
     monitor.update({env.now: queue.level})
     return monitor
+
+def calculate_tbs(sinr):
+    mapping=pd.read_csv('Data/sinr-tbs-mapping.csv')
+    tbs=mapping.iloc[sinr].values[1]
+    return tbs
 
 #scheduler takes packets from the queues according to the capacity of each user
 def scheduler(env, users, SCHEDULE_T):
     
+    prb_number=100
     counter=1 #counts the number of scheduling procedures
     alpha=-np.log10(0.01)/100
     
     while True: #größte Warteschlange wird auch bedient
+        
         yield env.timeout(SCHEDULE_T) #for each ms the scheduling is active -> per TTI
         metric=np.array([]) 
-        print(env.now)
-        users[0].mon= monitor(users[0].queue,users[0].mon)
-        users[1].mon= monitor(users[1].queue,users[1].mon)
-        users[2].mon= monitor(users[2].queue,users[2].mon)
+        users[0].mon= monitor(users[0].queue,users[0].mon,env)
+        users[250].mon= monitor(users[250].queue,users[250].mon,env)
+        users[450].mon= monitor(users[450].queue,users[450].mon,env)
         
-        for i in np.arange(np.size(users)):
-                  
+        for i in np.arange(np.size(users)): 
             if(users[i].qos==1):
                 metric=np.append(metric, (alpha*users[i].queue.level*(users[i].cp/users[i].mR)))  #list the metric of all UEs in the process 
                 users[i].mR=(1-1/counter)*users[i].mR #Ratenanpassung für alle Nutzer
-                
+            
             elif(users[i].qos==0):
-                metric=np.append(metric,(users[i].cp/users[i].mR))
-                users[i].mR=(1-1/counter)*users[i].mR #Ratenanpassung für alle Nutzer
-                
+                if(users[i].queue.level>0):
+                    metric=np.append(metric,(users[i].cp/users[i].mR))
+                    users[i].mR=(1-1/counter)*users[i].mR #Ratenanpassung für alle Nutzer
+                if(users[i].queue.level==0):
+                    metric=np.append(metric,0)
+                    users[i].mR=(1-1/counter)*users[i].mR #Ratenanpassung für alle Nutzer
+
         
-        sched_user_list = (-metric).argsort()[:5] # wählt die 5 UEs mit größter Metrik aus #find UE with maximal metric that will be used for scheduling
+        sched_user_list = (-metric).argsort() # wählt die 5 UEs mit größter Metrik aus #find UE with maximal metric that will be used for scheduling
         
-        print(sched_user_list)    
-        print(users[2].queue.level)
+        #print(sched_user_list)    
+        #print(users[2].queue.level)
+        remaining_prbs=prb_number
         
-        for k in [0,1,2,3,4,5,6,7]:
+        k=0
+        print('New scheduling round')
+        while(remaining_prbs>0):
             sched_user=sched_user_list[k]
-            sched_size=(round(users[sched_user].cp/8)) #scheduling size according to the channel capacity of the user -> user gets full bandwidth for 1ms (1TTI)
-            #users[sched_user].mon= monitor(users[sched_user].queue,users[sched_user].mon)
+
+            queue_size=users[sched_user].queue.level
+            tbs=users[sched_user].tbs
+
+            if((queue_size/tbs)<=remaining_prbs and queue_size>0):
+                sched_size=queue_size
+                remaining_prbs=remaining_prbs-np.ceil(queue_size/tbs)
+                
+            elif((queue_size/tbs)>remaining_prbs):
+                sched_size=remaining_prbs*tbs
+                remaining_prbs=remaining_prbs-sched_size
+                
+            elif(queue_size==0):
+                print('empty queue')
+                break
+            else:
+                print('something went wrong')
+            print('user:',sched_user)    
+            print('queue size before:',queue_size)
+            print('mR before:',users[sched_user].mR)
             users[sched_user].mR=users[sched_user].mR+(1/counter)*sched_size
             users[sched_user].queue.get(sched_size)
+            print('queue size after:',users[sched_user].queue.level)
+            print('mR afer:',users[sched_user].mR)
+            k=k+1
         counter=counter+1
-        #print(users[2].mR)
-        #print(users[2].cp)
-        #print(env.now)
+        
 
         
             
@@ -58,9 +90,11 @@ class ue:
     def __init__(self,sinr,sinr2,cell1,cell2,env,qos):
         self.sinr=sinr
         self.sinr2=sinr2
+        self.tbs=calculate_tbs(sinr)
+        self.tbs2=calculate_tbs(sinr2)
         self.qos=qos
-        self.cp=0.7*20000000*np.log2(1+np.power(10,sinr/10))/8000 #division by 8000 to determine number of bits that can be transmitted per TTI (1ms)
-        self.cp2=0.7*20000000*np.log2(1+np.power(10,sinr2/10))/8000
+        self.cp=0.5*0.7*20000000*np.log2(1+np.power(10,sinr/10))/8000 #division by 8000 to determine number of bits that can be transmitted per TTI (1ms)
+        self.cp2=0.5*0.7*20000000*np.log2(1+np.power(10,sinr2/10))/8000
         self.cell1=cell1
         self.cell2=cell2
         self.mR=0.1 #mittlere Rate
@@ -74,7 +108,7 @@ class ue:
         while True:
             if(on_off==1 and counter<3000):
                 self.queue.put(160) #20 bytes
-                mon= monitor(self.queue,self.mon)
+                mon= monitor(self.queue,self.mon,env)
                 #yield env.timeout(poisson.rvs(6, 1))
                 #print('On Phase')
                 #print(self.queue.level)
@@ -83,8 +117,7 @@ class ue:
                 yield env.timeout(20) #every 20ms new packet
             elif(on_off==0):
                 on_off=1
-                #print('Off Phase')
-                #print(self.queue.level)
+
                 yield env.timeout(3000) #3s no packet to be sent
             elif(on_off==1 and counter>=3000):
                 on_off=0
@@ -93,9 +126,7 @@ class ue:
             
     def user_packets(self,env):
         while True:
-            print('normal ue')
             #print('o-user')
             self.queue.put(2000)
-            #self.mon= monitor(self.queue,self.mon)
             yield env.timeout(poisson.rvs(500, 1))
     
